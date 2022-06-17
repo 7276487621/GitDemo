8baba01f437c566fc2c03e19b1701145faeba66f
sachin <gardisachin0@gmail.com>
2022-06-17 14:42:03 +0530

    This is commit from asia time zone guy

--- src/test/java/qaclickacademy/Mavenjava/RestApiTest.java
+++ src/test/java/qaclickacademy/Mavenjava/RestApiTest.java
@@ -7,6 +7,8 @@ public class RestApiTest {
 	@Test
 	public void postJiraTest() {
 		System.out.println("postJiraTest");
+		System.out.println("postJiraTest2");
+		System.out.println("postJiraTest3");
 	}
 	@Test
 	public void deleteTwitterTest() {
